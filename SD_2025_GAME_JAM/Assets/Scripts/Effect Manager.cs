3a4721c3727d57912e1f5d6f5c215375d910d85e
lxrdflame <tyrique.s.r@gmail.com>
2025-07-11 11:33:22 +0200

    Effect script

--- SD_2025_GAME_JAM/Assets/Scripts/Effect Manager.cs
+++ SD_2025_GAME_JAM/Assets/Scripts/Effect Manager.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EffectManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   private CharacterControls characterController;
+
+    private void Start()
     {
-        
+        characterController = GetComponent<CharacterControls>();
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Graviole()
     {
-        
+        yield return new WaitForSeconds(0);
+        characterController.gravity = characterController.gravity * -1; 
+        yield return new WaitForSeconds(1);
+        characterController.gravity = characterController.gravity * -1;
+
+    }
+
+    public void Freeze()
+    {
+
+    }
+
+    public void Teleport()
+    {
+
+    }
+
+    public void FlashBang()
+    {
+
+    }
+
+    public void SlimeSling()
+    {
+
+    }
+
+    public void Burnt()
+    {
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.CompareTag("Flame"))
+        {
+            Destroy(hit.collider.gameObject);
+            StartCoroutine(Graviole());
+        }
+    }
+
+
+    //Side Effects
+    public void SmokeScreen()
+    {
+
+    }
+
+    public void BlackCloud()
+    {
+
+    }
+
+    public void ScreenShake()
+    {
+
+    }
+
+    public void Burn()
+    {
+
+    }
+
+    public void ShakeLevel()
+    {
+
     }
 }
