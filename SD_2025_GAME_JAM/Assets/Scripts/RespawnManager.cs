d1cd3155a929122bc58b751e61432777530beb84
profoundBoi <connor170103@gmail.com>
2025-07-12 12:53:12 +0200

    after sub

--- SD_2025_GAME_JAM/Assets/Scripts/RespawnManager.cs
+++ SD_2025_GAME_JAM/Assets/Scripts/RespawnManager.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.SearchService;
+
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
